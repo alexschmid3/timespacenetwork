043cdd19c0543be50828b03d5014d3ccbb702e92
Alex Schmid <alexandriaschmid3@gmail.com>
2023-03-10 15:28:41 -0500

    Add time-space network code

---
+++ run_all.jl
@@ -0,0 +1,41 @@
+
+using JuMP, Gurobi, Random, CSV, DataFrames, Statistics, Dates 
+
+#-------------------------------------LOAD SCRIPTS-------------------------------------#
+
+include("scripts/createnetwork.jl")
+
+#----------------------------------NETWORK PARAMETERS----------------------------------#  	
+
+#Read experiment parameters 
+horizon = 168									#Length of time horizon in hours 
+tstep = 6										#Time discretization
+numlocs = 30										#Number of physical locations, can choose from 1 to 66 for this dataset
+locationfilename = "data/locations.csv"
+arcfilename = "data/arcs.csv"
+randomseedval = 1905							#Set random seed if there are any random components of an algorithm
+Random.seed!(randomseedval)
+
+#-----------------------------------GENERATE NETWORK-----------------------------------# 
+
+#Create node and arc networks
+tsnetwork = createfullnetwork(locationfilename, arcfilename, numlocs, horizon, tstep)
+
+#Print some fun facts
+println("Initialized time space network with...")
+println("Num nodes = ", tsnetwork.numnodes)
+println("Num arcs = ", tsnetwork.numarcs)
+
+# All attributes of tsnetwork:
+# ---------------------------------#
+# loccoords --> array of coordinates for all locations
+# numnodes --> total number of nodes
+# nodeid --> dictionary, nodes[l, t] = node id for node representing location l at time t 
+# nodedesc --> dictionary, nodelookup[n] = (location, time) of node n
+# times --> array of discretized time periods
+# numarcs --> total number of arcs 
+# arcid --> dictionary, arcs[n1, n2] = arc id for arc starting at n1 and ending at n2 (if it exists)
+# arcdesc --> dictionary, arclookup[a] = (startnode, endnode) of arc a
+# A_plus --> dictionary, A-plus[n] = list of arcs with startnode n (useful for flow-balance constraints)
+# A_minus --> dictionary, A_minus[n] = list of arcs with endnode n (useful for flow-balance constraints)
+# arccost --> array of cost/distance of each arc
---
+++ scripts/createnetwork.jl
@@ -0,0 +1,114 @@
+
+#Reads the location file and returns the location coordinates
+function readlocations(locationfilename, numlocs)
+
+	data = CSV.read(locationfilename, DataFrame)
+	
+	loccoords = hcat(data[:,2], data[:,3])[1:numlocs,:]
+	
+	return loccoords
+
+end
+
+#-----------------------------------------------------------------------------------#
+
+#Create the time-space nodes, returning the number, ids, and descriptions
+function createtimespacenodes(numlocs, horizon, tstep)
+
+	nodeid, nodedesc = Dict(), Dict()
+
+	index = 1
+	for t in 0:tstep:horizon
+		for l in 1:numlocs
+			nodeid[l,t] = index
+			nodedesc[index] = (l,t)
+			index += 1
+		end	
+	end
+	
+	numnodes = length(nodeid)
+	times = [t for t in 0:tstep:horizon]
+
+	return numnodes, nodeid, nodedesc, times
+
+end
+
+#-----------------------------------------------------------------------------------#
+
+#Read the list of arcs from the arc file
+function getphysicalarcs(arcfilename, tstep)
+
+	data = CSV.read(arcfilename, DataFrame)
+	
+	physicalarcs = []
+
+	for a in 1:size(data)[1]
+		l1, l2 = data[a,1], data[a,2]
+		if (l1 <= numlocs) & (l2 <= numlocs)
+			arcdistance = data[a,4]
+			arclength_raw = data[a,3]
+			arclength_discretized = tstep * ceil(arclength_raw / tstep)
+			push!(physicalarcs, (l1, l2, arcdistance, arclength_raw, arclength_discretized))
+		end
+	end
+
+	return physicalarcs
+
+end
+
+#-----------------------------------------------------------------------------------#
+
+#Create the time-space network arcs, returning the number, ids, descriptions, and cost of each arc
+function createtimespacearcs(physicalarcs, numlocs, numnodes, nodeid)
+
+	arcid, arcdesc, A_plus, A_minus, arccost = Dict(), Dict(), Dict(), Dict(), []
+	
+	for node in 1:numnodes
+		A_plus[node] = []
+		A_minus[node] = []
+	end
+
+	stationaryarcs = []
+	for l in 1:numlocs
+		push!(stationaryarcs, (l, l, 0, tstep, tstep))
+	end
+
+	index = 1
+	for arc in union(physicalarcs, stationaryarcs), t in 0:tstep:horizon-arc[5]
+		startnode = nodeid[arc[1],t]
+		endnode = nodeid[arc[2],t+arc[5]]
+	
+		arcid[(startnode,endnode)] = index
+		arcdesc[index] = (startnode,endnode)
+		
+		push!(A_plus[startnode], index)
+		push!(A_minus[endnode], index)
+
+		push!(arccost, arc[3])
+
+		index += 1
+	end
+
+	numarcs = length(arcid)
+
+	return numarcs, arcid, arcdesc, A_plus, A_minus, arccost
+
+end
+
+#-----------------------------------------------------------------------------------#
+
+#Build the full time-space network
+function createfullnetwork(locationfilename, arcfilename, numlocs, horizon, tstep)
+
+	#Build network
+	loccoords = readlocations(locationfilename, numlocs)
+	numnodes, nodeid, nodedesc, times = createtimespacenodes(numlocs, horizon, tstep)
+	physicalarcs = getphysicalarcs(arcfilename, tstep)
+	numarcs, arcid, arcdesc, A_plus, A_minus, arccost = createtimespacearcs(physicalarcs, numlocs, numnodes, nodeid)
+
+	#Create a NamedTuple with all the useful network data/parameters
+	tsnetwork = (loccoords=loccoords, numnodes=numnodes, nodeid=nodeid, nodedesc=nodedesc, times=times, numarcs=numarcs, arcid=arcid, arcdesc=arcdesc, A_plus=A_plus, A_minus=A_minus, arccost=arccost)
+
+	return tsnetwork
+
+end
